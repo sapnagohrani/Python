44dce0b6e7108f9e8e90ca39db69c2efc9d629ac
Sapna Gohrani <Sapna.Gohrani2@mindtree.com>
2020-08-03 01:50:49 +0530

    Bubble Sort

---
+++ Sorting/BubbleSort.py
@@ -0,0 +1,12 @@
+def bubble_sort(array):
+    for i in range(len(array)):
+        for j in range(0, len(array) - i - 1):
+            if array[j] > array[j + 1]:
+                temp = array[j]
+                array[j] = array[j + 1]
+                array[j + 1] = temp
+    return array
+
+
+arr = [-8, 100, 25, 50, 11, 1, 0]
+print(bubble_sort(arr))
