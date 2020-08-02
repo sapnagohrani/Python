97f28f82d567675dd92d532b2202050a013412b1
Sapna Gohrani <Sapna.Gohrani2@mindtree.com>
2020-08-03 02:56:07 +0530

    Bubble Sort Optimization with swap variable

--- Sorting/BubbleSort.py
+++ Sorting/BubbleSort.py
@@ -1,8 +1,12 @@
 def bubble_sort(array):
     for i in range(len(array)):
+        swap = False
         for j in range(0, len(array) - i - 1):
             if array[j] > array[j + 1]:
                 array[j], array[j + 1] = array[j + 1], array[j]
+                swap = True
+        if not swap:
+            break
     return array
 
 
