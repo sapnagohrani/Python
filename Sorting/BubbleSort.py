cef28d42a2da410870061e422111f1c07706258a
Sapna Gohrani <Sapna.Gohrani2@mindtree.com>
2020-08-03 02:44:18 +0530

    Bubble Sort swap in one line

--- Sorting/BubbleSort.py
+++ Sorting/BubbleSort.py
@@ -2,9 +2,7 @@ def bubble_sort(array):
     for i in range(len(array)):
         for j in range(0, len(array) - i - 1):
             if array[j] > array[j + 1]:
-                temp = array[j]
-                array[j] = array[j + 1]
-                array[j + 1] = temp
+                array[j], array[j + 1] = array[j + 1], array[j]
     return array
 
 
